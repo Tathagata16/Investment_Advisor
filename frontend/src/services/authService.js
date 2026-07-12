b78485609791809cd33c8c413aba4a0c88e48106
Tathagata16 <tathagataghosh1609@gmail.com>
2026-07-12 17:07:40 +0530

    debug

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,12 +1,12 @@
 import api from "./api";
 
 export const signup = async (data) => {
-  const response = await api.post("/auth/v1/signup", data);
+  const response = await api.post("/auth/signup", data);
   return response.data;
 };
 
 export const login = async (data) => {
-  const response = await api.post("/auth/v1/login", data);
+  const response = await api.post("/auth/login", data);
   return response.data;
 };
 
